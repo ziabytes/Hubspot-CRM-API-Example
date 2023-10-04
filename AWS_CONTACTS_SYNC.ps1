0c89f190f3ab1543d2de526f74e77070486b16c5
ziabytes <ziabytes@gmail.com>
2023-10-04 07:03:39 -0400

    additional minor commenting on the requests half
    added some minor commenting

--- AWS_CONTACTS_SYNC.ps1
+++ AWS_CONTACTS_SYNC.ps1
@@ -53,51 +53,107 @@ foreach ($record_without_email in $transformed_data_with_empty_email) {
     }
 }
 
-# Create new users in HubSpot
+# Array to store results of create requests
 $create_request_results = @()
-foreach ($user in $users_to_create) {
-    $properties_to_create = @{}
-    if (![string]::IsNullOrWhiteSpace($user.email)) { $properties_to_create.Add("email", $user.email) }
-    if (![string]::IsNullOrWhiteSpace($user.phone)) { $properties_to_create.Add("phone", $user.phone) }
-    if (![string]::IsNullOrWhiteSpace($user.firstname)) { $properties_to_create.Add("firstname", $user.firstname) }
-    if (![string]::IsNullOrWhiteSpace($user.lastname)) { $properties_to_create.Add("lastname", $user.lastname) }
 
-    $user_payload = @{ properties = $properties_to_create } | ConvertTo-Json
+# Iterate through users to create and send POST requests to HubSpot
+foreach ($user in $users_to_create) {
+    # Initialize payload, explicitly checking each property for null or empty values
+    $properties_to_create = @{}
+    if (![string]::IsNullOrWhiteSpace($user.email)) {
+        $properties_to_create.Add("email", $user.email)
+    }
+    if (![string]::IsNullOrWhiteSpace($user.phone)) {
+        $properties_to_create.Add("phone", $user.phone)
+    }
+    if (![string]::IsNullOrWhiteSpace($user.firstname)) {
+        $properties_to_create.Add("firstname", $user.firstname)
+    }
+    if (![string]::IsNullOrWhiteSpace($user.lastname)) {
+        $properties_to_create.Add("lastname", $user.lastname)
+    }
+
+    $user_payload = @{
+        properties = $properties_to_create
+    } | ConvertTo-Json
+
+    # Send POST request to create user
     try {
         $create_user = Invoke-RestMethod -Uri $hubspot_contacts_endpoint -Method Post -Headers $hubspot_data_headers -Body $user_payload -ContentType 'application/json'
         $status = "Success"
     } catch {
         $status = "Fail"
     }
-    $create_request_results += [PSCustomObject]@{ email = $user.email; firstname = $user.firstname; lastname = $user.lastname; status = $status }
+
+    # Store the result in the array
+    $create_request_results += [PSCustomObject]@{
+        email     = $user.email
+        firstname = $user.firstname
+        lastname  = $user.lastname
+        status    = $status
+    }
+
     Start-Sleep -Milliseconds 250
 }
 
-# Update existing users in HubSpot
+# Array to store results of update requests
 $update_request_results = @()
+
+# Iterate through existing users to update them in HubSpot
 foreach ($existing_user in $existing_users) {
+    # Initialize user_id as an empty string
     $user_id = ""
+
+    # Find the corresponding user ID based on email, if email is not null
     if (![string]::IsNullOrWhiteSpace($existing_user.email)) {
         $user_id = ($get_crm_contact_data.results.Where({ $_.properties.email -eq $existing_user.email })).id
     }
+
+    # If user_id is still empty, try finding it based on firstname and lastname
     if ($user_id -eq "") {
         $user_id = ($get_crm_contact_data.results.Where({ $_.properties.firstname -eq $existing_user.firstname -and $_.properties.lastname -eq $existing_user.lastname })).id
     }
-    if ($user_id -eq "") { continue }
 
+    # Skip the loop iteration if the user ID is not found
+    if ($user_id -eq "") {
+        continue
+    }
+
+    # Initialize payload, explicitly checking each property for null or empty values
     $properties_to_update = @{}
-    if (![string]::IsNullOrWhiteSpace($existing_user.email)) { $properties_to_update.Add("email", $existing_user.email) }
-    if (![string]::IsNullOrWhiteSpace($existing_user.phone)) { $properties_to_update.Add("phone", $existing_user.phone) }
-    if (![string]::IsNullOrWhiteSpace($existing_user.firstname)) { $properties_to_update.Add("firstname", $existing_user.firstname) }
-    if (![string]::IsNullOrWhiteSpace($existing_user.lastname)) { $properties_to_update.Add("lastname", $existing_user.lastname) }
+    if (![string]::IsNullOrWhiteSpace($existing_user.email)) {
+        $properties_to_update.Add("email", $existing_user.email)
+    }
+    if (![string]::IsNullOrWhiteSpace($existing_user.phone)) {
+        $properties_to_update.Add("phone", $existing_user.phone)
+    }
+    if (![string]::IsNullOrWhiteSpace($existing_user.firstname)) {
+        $properties_to_update.Add("firstname", $existing_user.firstname)
+    }
+    if (![string]::IsNullOrWhiteSpace($existing_user.lastname)) {
+        $properties_to_update.Add("lastname", $existing_user.lastname)
+    }
 
-    $user_payload = @{ properties = $properties_to_update } | ConvertTo-Json
+    $user_payload = @{
+        properties = $properties_to_update
+    } | ConvertTo-Json
+
+    # Send PATCH request to update user
     try {
         $update_user = Invoke-RestMethod -Uri "$hubspot_contacts_endpoint/$user_id" -Method Patch -Headers $hubspot_data_headers -Body $user_payload -ContentType 'application/json'
         $status = "Success"
     } catch {
         $status = "Fail"
     }
-    $update_request_results += [PSCustomObject]@{ id = $user_id; email = $existing_user.email; firstname = $existing_user.firstname; lastname = $existing_user.lastname; status = $status }
+
+    # Store the result in the array
+    $update_request_results += [PSCustomObject]@{
+        id        = $user_id
+        email     = $existing_user.email
+        firstname = $existing_user.firstname
+        lastname  = $existing_user.lastname
+        status    = $status
+    }
+
     Start-Sleep -Milliseconds 250
 }
